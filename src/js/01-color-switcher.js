ecac66639bd83037bdf9f1527b9537b98ea7d363
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-03 12:29:06 +0200

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStartSwitch = document.querySelector('[data-start]');
+const btnStopSwitch = document.querySelector('[data-stop]');
 
+btnStopSwitch.disabled = true;
+let colorTimer;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const switchBcg = () => {
+    colorTimer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStartSwitch.disabled = true;
+    btnStopSwitch.disabled = false;
+};
+
+btnStartSwitch.addEventListener("click", switchBcg);
+
+const stopSwitcher = () => {
+    btnStartSwitch.disabled = false;
+    btnStopSwitch.disabled = true;
+    clearInterval(colorTimer);
+}
+
+btnStopSwitch.addEventListener("click", stopSwitcher);
