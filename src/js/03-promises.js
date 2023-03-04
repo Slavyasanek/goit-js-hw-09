9be89fb50fed86252c9d81f8bed25cb8fa55774b
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-04 15:39:36 +0200

    add promises generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const formInput = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay)
+  })
 }
+
+const sumbitPromises = (event) => {
+  event.preventDefault();
+  const amount = Number.parseInt(formInput.amount.value);
+  let delay = Number.parseInt(formInput.delay.value);
+  const step = Number.parseInt(formInput.step.value);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${position} in ${delay}ms`,
+          {
+              timeout: 2000,
+              cssAnimationStyle: 'zoom',
+          },
+      );})
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${position} in ${delay}ms`,
+          {
+              timeout: 2000,
+              cssAnimationStyle: 'zoom',
+          },
+      );
+      });
+      delay += step;
+  }
+  event.currentTarget.reset();
+};
+
+formInput.addEventListener("submit", sumbitPromises);
